59caa5406fbdfeaed3b69c193f1709c898710e28
ypt19778 <littlebigleimicrosft@gmail.com>
2025-10-26 16:33:15 -0400

    serializing file

---
+++ serializement.lua
@@ -0,0 +1,56 @@
+local M = {}
+M.file = nil
+
+function M:setFile(file)
+    self.currentFile = tostring(file)
+end
+
+function M:unsetFile()
+    self.currentFile = nil
+end
+
+function M:resetFile()
+    love.filesystem.write(self.currentFile, "")
+end
+
+function M:setCleanFile(file)
+    self.currentFile = file
+    M:resetFile(file)
+end
+
+function M:serialize(o)
+    local tab = "    "
+    if type(o) == 'number' then
+        love.filesystem.append(self.currentFile, o)
+    elseif type(o) == 'string' then
+        love.filesystem.append(self.currentFile, string.format("%q", o))
+    elseif type(o) == 'table' then
+        love.filesystem.append(self.currentFile, "{\n")
+        for k, v in pairs(o) do
+            love.filesystem.append(self.currentFile, tab..k.." = ")
+            self:serialize(v)
+            love.filesystem.append(self.currentFile, ",\n")
+        end
+        love.filesystem.append(self.currentFile, "}\n")
+    else 
+        error('cannot serialize a '..type(o))
+    end
+end
+
+function M:deserialize(file)
+    return love.filesystem.read(file or self.currentFile)
+end
+
+function M:translate(o)
+    return assert(load("return "..o))()
+end
+
+--[[
+function M:deserializeAndTranslate()
+    local o_data = M:deserialize()
+    local o = M:translate(o_data)
+    return o
+end
+]]
+
+return M
